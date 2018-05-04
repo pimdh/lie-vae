7be8213e50439e67b18c68ba1af1179d3c35cb96
trdavidson <itimrd@gmail.com>
2018-05-04 13:47:02 +0200

    cuda fun in reparam

--- vae/vae.py
+++ vae/vae.py
@@ -3,29 +3,34 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
+
 from SNets import S2ConvNet, S2DeconvNet
 from pytorch_util import MLP
-from reparameterize import Nreparametrize
+from reparameterize import Nreparameterize
+from reparameterize import SO3reparameterize
 from pytorch_util import logsumexp
+
+from s2cnn.nn.soft.so3_integrate import so3_integrate
 
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
         self.encoder = None
         self.decoder = None
-        self.reparametrize = []
+        self.reparameterize = []
         self.r_callback = []
 
     def encode(self, x, n=1):
         h = self.encoder(x)
-        z = [r(f(h), n) for r, f in zip(self.reparametrize, self.r_callback)]
+        z = [r(f(h), n) for r, f in zip(self.reparameterize, self.r_callback)]
 
         return z
 
     def kl(self):
         # NOTE always call after encode
         # TODO make this bahaviour impossible
-        kl = [r.kl() for r in self.reparametrize]
+        kl = [r.kl() for r in self.reparameterize]
         return kl
 
     def decode(self, z):
@@ -42,56 +47,69 @@ class VAE(nn.Module):
     def recon_loss(self, x, x_recon):
         raise NotImplemented
 
-    def elbo(self, x):
-        x_recon = self.forward(x)[0]
+    def elbo(self, x, n=1):
+        x_recon = self.forward(x, n)
         kl = self.kl()
         # TODO maybe sum directly  without stacking 
         kl_summed = torch.sum(torch.stack(kl, -1), -1)
-        recon_loss = self.recon_loss(x, x_recon)
+        recon_loss = self.recon_loss(x_recon, x)
         return recon_loss, kl_summed
 
     def log_likelihood(self, x, n=1):
         raise NotImplemented
 
 
+class equivariant_callback(nn.Module):
+    def __init__(self, specs):
+        super(equivariant_callback)
+    
 class NS2VAE(VAE):
     def __init__(self, z_dim=10,
                  encoder_f=[1, 10, 10],
                  decoder_f=[10, 10, 1],
                  encoder_b=[30, 20, 6],
                  decoder_b=[5, 15, 30],
-                 mlp_h=[100]):
+                 encoder_mlp_h=[100],
+                 decoder_mlp_h=[100]):
         super(NS2VAE, self).__init__()
 
         self.encoder = S2ConvNet(f_list=encoder_f, b_list=encoder_b)
-        self.decoder = S2DeconvNet(f_list=decoder_f, b_list=decoder_b, mlp_dim=[z_dim])
+        self.decoder_mlp_h = decoder_mlp_h.copy()
+        self.decoder_mlp_h.insert(0, z_dim)
+        self.decoder = S2DeconvNet(f_list=decoder_f, b_list=decoder_b, mlp_dim=self.decoder_mlp_h)
 
-        self.mlp_h = mlp_h.copy()
-        self.mlp_h.insert(0, encoder_f[-1] * (encoder_b[-1] * 2) ** 3)
-        self.mlp = MLP(H=self.mlp_h, end_activation=True)
+        # output of the encoder base
+        encoder_final_dim = encoder_f[-1] * (encoder_b[-1] * 2) ** 3
+        
+        self.encoder_mlp_h = encoder_mlp_h.copy()
+        self.encoder_mlp_h.insert(0, encoder_final_dim)
+        self.encoder_mlp = MLP(H=self.encoder_mlp_h, end_activation=True)
 
-        self.repar1 = Nreparametrize(mlp_h[-1], z_dim)
-        self.reparametrize = [self.repar1]
-        self.r_callback = [lambda x: self.mlp(x.view(-1, self.mlp_h[0]))]
+#         self.repar1 = Nreparameterize(encoder_mlp_h[-1], z_dim)
+        self.repar2 = SO3reparameterize(encoder_mlp_h[-1], z_dim)
+        self.reparameterize = [self.repar2]
+        
+        callback1 = (lambda x: MLP(so3_integrate(x), H=self.encoder_mlp_h, end_activation=True))
+        
+        self.r_callback = [lambda x: self.encoder_mlp(x.view(-1, self.encoder_mlp_h[0]))]
 
         self.bce = nn.BCELoss(size_average=False)
-
-    def recon_loss(self, x, x_recon):
+        
+        
+    def recon_loss(self, x_recon, x):
         x = x.expand_as(x_recon)
-        x_recon = F.sigmoid(x_recon)
-        b = x_recon.log() * x + (1 - x_recon).log() * (1 - x)
-#         b = F.binary_cross_entropy_with_logits(x_recon, x) * (x_recon.size()[-1] ** 2)
-        return b.sum(-1).sum(-1).sum(-1)
+        max_val = (-x_recon).clamp(min=0)
+        loss = x_recon - x_recon * x + max_val + ((-max_val).exp() + \
+                                               (-x_recon - max_val).exp()).log()
+        
+        return loss.sum(-1).sum(-1).sum(-1)
     
     
     def log_likelihood(self, x, n=1):
-
         x_recon = self.forward(x, n)
+  
+        log_p_z = torch.cat([r.log_prior() for r in self.reparameterize], -1).sum(-1)
+        log_q_z_x = torch.cat([r.log_posterior() for r in self.reparameterize], -1).sum(-1)
+        log_p_x_z = - self.recon_loss(x_recon, x)
         
-        log_p_z = torch.cat([r.log_prior().sum(-1) for r in self.reparametrize], -1).sum(-1)
-        
-        log_q_z_x = torch.cat([r.log_posterior().sum(-1) for r in self.reparametrize], -1).sum(-1)
-    
-        log_p_x_z = - self.recon_loss(x, x_recon)
-
         return (logsumexp(log_p_x_z + log_p_z - log_q_z_x, dim=0) - np.log(n)).mean()
