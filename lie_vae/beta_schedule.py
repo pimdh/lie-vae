65a96e9748b38d59f045339bcffcb6490e5e9592
Pim de Haan <pimdehaan@gmail.com>
2018-05-20 18:40:35 -0700

    Added R schedule

--- lie_vae/beta_schedule.py
+++ lie_vae/beta_schedule.py
@@ -38,6 +38,8 @@ def get_beta_schedule(schedule, beta):
         return LinearSchedule(0.001, 100, 60000, 120000)
     elif schedule == 'q':
         return LinearSchedule(0.001, 10, 60000, 240000)
+    elif schedule == 'r':
+        return LinearSchedule(0.001, 30, 60000, 600000)
     else:
         raise RuntimeError('Wrong beta schedule. Schedule={}, beta={}'
                            .format(schedule, beta))
